cb8805770c26c43057c32875327e1baf5e5e387f
quadroloop <nplixel@gmail.com>
2018-01-22 15:09:03 +0800

    silmulated template generation of db panel

--- nebular.php
+++ nebular.php
@@ -74,7 +74,8 @@ https://github.com/quadroloop/nebular
   <div id="editor"></div>
   <div id="dashboard">
     <div class="w3-row w3-small">
-      <div class="w3-container dark-border w3-col s4" style="border-radius:5px;width:350px;margin:10px;background-color:#272822;">
+      <!-- start of Db cell-->
+      <div class="w3-container dark-border w3-col s4 db-panel">
     <a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
     <hr style="border-top: 0.4px solid #333;">
     <div class="w3-bar">
@@ -83,7 +84,224 @@ https://github.com/quadroloop/nebular
      <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
     </div>
     <br>
-      </div>
+    </div>
+    <!--end of db cell-->
+    <!-- start of Db cell-->
+    <div class="w3-container dark-border w3-col s4 db-panel">
+  <a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+  <hr style="border-top: 0.4px solid #333;">
+  <div class="w3-bar">
+    <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+   <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+   <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+  </div>
+  <br>
+  </div>
+  <!--end of db cell-->
+  <!-- start of Db cell-->
+  <div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+  <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+ <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+ <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+  <!-- start of Db cell-->
+  <div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+  <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+ <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+ <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
+<!-- start of Db cell-->
+<div class="w3-container dark-border w3-col s4 db-panel">
+<a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> Sample_DB</a><br><br>
+<hr style="border-top: 0.4px solid #333;">
+<div class="w3-bar">
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a>
+<a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a>
+</div>
+<br>
+</div>
+<!--end of db cell-->
     </div>
   </div>
 
