384813615d446a89d0edb48e2fc54e0dbd7900db
quadroloop <nplixel@gmail.com>
2018-01-23 08:23:51 -0800

    added axios.js and loading content using axios via get request, pretty slow.

--- nsrc/js/nebular.js
+++ nsrc/js/nebular.js
@@ -80,3 +80,33 @@ function search(data,pointer) {
     }
 }
 }
+
+// load table contents
+
+axios.get('https://jsonplaceholder.typicode.com/posts/')
+  .then(function (response) {
+    var tablec = response.data;
+    for(var i=0;i<tablec.length;i++) {
+  document.getElementById('dbtable').innerHTML += '<tr><td>'+tablec[i].title+'</td><td>Smith</td><td>50</td><td>Jill</td><td>Smith</td><td>50</td><td>Jill</td><td>Smith</td><td>50</td><td>Jill</td><td>Smith</td><td>50</td></tr>';
+}
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+// load db panel contents
+
+axios.get('https://jsonplaceholder.typicode.com/posts/')
+  .then(function (response) {
+    var tablec = response.data;
+    for(var i=0;i<tablec.length;i++) {
+  document.getElementById('dtable').innerHTML += '<li><div class="w3-container dark-border w3-col s4 db-panel w3-animate-opacity"><a id="tool_state" class="dark-border w3-round w3-padding-small w3-text-blue tool_state"><i class="fa fa-database w3-text-blue"></i> '+tablec[i].title+'</a><br><br> <hr style="border-top: 0.4px solid #333;"> <div class="w3-bar"> <a onclick="nav(&apos;datatable&apos;);cdb=&apos;dbtable&apos;;pointer=&apos;table&apos;" class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-bars w3-text-indigo"></i> View Database</a> <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-trash w3-text-red"></i> Delete</a> <a class="w3-bar-item w3-text-grey w3-btn w3-hover-blue w3-round"><i class="fa fa-upload w3-text-amber"></i> Export</a> </div> <br> </div> </li>';
+}
+
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+ 
