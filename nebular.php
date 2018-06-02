a408a91e210aefed40295b2f9fc0b2a9aee49862
quadroloop <nplixel@gmail.com>
2018-06-02 10:14:38 +0300

    added gitignore file

--- nebular.php
+++ nebular.php
@@ -716,13 +716,15 @@ function qcolor($req){
                     <div class="col-md-12">
                         <div class="card w3-card-4">
                             <div class="header">
-                                <h4 class="title"><i class="ti-bolt"></i> Realtime queries</h4>
+                                <h4 class="title"><i class="ti-bolt"></i> Query Overview</h4>
                                 <p class="category">Update Realtime</p>
                             </div>
                             <div class="content">
                               <!-- data -->
-                                  <div class="w3-round w3-black w3-container w3-padding w3-small" style="height:220px;">
-                                    <a class="w3-text-green"><i class="ti-bolt"></i> GET : http://192.168.1.6:80/nebular.php?api=createDB </a>
+                                  <div class="w3-round w3-white w3-container w3-padding w3-small">
+                                    <div id="container" style="width: 100%;">
+                                    <canvas id="canvas"></canvas>
+                                    </div>
                                   </div>
                               <!-- data end -->
                                 <div class="footer">
@@ -737,6 +739,8 @@ function qcolor($req){
                 </div>
             </div>
         </div>
+        <script src="./nebular-src/js/chart.min.js"></script>
+        <script src="./nebular-src/js/chart.utils.js"></script>
         <script type="text/javascript">
              setInterval(function(){
                      axios.get('nebular.php?api=getStats')
@@ -751,7 +755,120 @@ function qcolor($req){
 
              },200);
 
-        
+            var MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var color = Chart.helpers.color;
+    var barChartData = {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+      datasets: [{
+        label: 'Dataset 1',
+        backgroundColor: color(window.chartColors.red).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.red,
+        borderWidth: 1,
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ]
+      }, {
+        label: 'Dataset 2',
+        backgroundColor: color(window.chartColors.blue).alpha(0.5).rgbString(),
+        borderColor: window.chartColors.blue,
+        borderWidth: 1,
+        data: [
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor(),
+          randomScalingFactor()
+        ]
+      }]
+
+    };
+
+    window.onload = function() {
+      var ctx = document.getElementById('canvas').getContext('2d');
+      window.myBar = new Chart(ctx, {
+        type: 'bar',
+        data: barChartData,
+        options: {
+          responsive: true,
+          legend: {
+            position: 'top',
+          },
+          title: {
+            display: true,
+            text: 'Recent 100 Requests'
+          }
+        }
+      });
+
+    };
+
+    document.getElementById('randomizeData').addEventListener('click', function() {
+      var zero = Math.random() < 0.2 ? true : false;
+      barChartData.datasets.forEach(function(dataset) {
+        dataset.data = dataset.data.map(function() {
+          return zero ? 0.0 : randomScalingFactor();
+        });
+
+      });
+      window.myBar.update();
+    });
+
+    var colorNames = Object.keys(window.chartColors);
+    document.getElementById('addDataset').addEventListener('click', function() {
+      var colorName = colorNames[barChartData.datasets.length % colorNames.length];
+      var dsColor = window.chartColors[colorName];
+      var newDataset = {
+        label: 'Dataset ' + barChartData.datasets.length,
+        backgroundColor: color(dsColor).alpha(0.5).rgbString(),
+        borderColor: dsColor,
+        borderWidth: 1,
+        data: []
+      };
+
+      for (var index = 0; index < barChartData.labels.length; ++index) {
+        newDataset.data.push(randomScalingFactor());
+      }
+
+      barChartData.datasets.push(newDataset);
+      window.myBar.update();
+    });
+
+    function addData() {
+      if (barChartData.datasets.length > 0) {
+        var month = MONTHS[barChartData.labels.length % MONTHS.length];
+        barChartData.labels.push(month);
+
+        for (var index = 0; index < barChartData.datasets.length; ++index) {
+          // window.myBar.addData(randomScalingFactor(), index);
+          barChartData.datasets[index].data.push(randomScalingFactor());
+        }
+
+        window.myBar.update();
+      }
+    }
+
+    document.getElementById('removeDataset').addEventListener('click', function() {
+      barChartData.datasets.splice(0, 1);
+      window.myBar.update();
+    });
+
+    document.getElementById('removeData').addEventListener('click', function() {
+      barChartData.labels.splice(-1, 1); // remove the label first
+
+      barChartData.datasets.forEach(function(dataset) {
+        dataset.data.pop();
+      });
+
+      window.myBar.update();
+    });
         </script>     
        <?php endif; ?>
 
